821c66a650affeba95d36f09f34af36ab1b9aac6
白川遥平 <youhei@youhei.local>
2019-11-25 00:15:40 +0900

    正の状態に戻した

--- src/test/java/org/acme/ExampleResourceTest.java
+++ src/test/java/org/acme/ExampleResourceTest.java
@@ -15,7 +15,7 @@ public class ExampleResourceTest {
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("hellodayo"));
+             .body(is("hello"));
     }
 
 }
